5b8bc95b0f54aac6ffc68091d6251098ebc2c05e
Laura Musso <lauramussopd@gmail.com>
2024-04-09 20:33:14 +0200

    fix thew wrong password

--- src/components/taskDetails.vue
+++ src/components/taskDetails.vue
@@ -13,7 +13,7 @@ const taskStore = useTaskStore();
 
 const newTaskTitle = ref('')
 const _isEditing = ref(false)
-const taskStatus = computed(() => `check_box${props.task.is_complete ? '' : '_outline_blank'}` )
+// const taskStatus = computed(() => `check_box${props.task.is_complete ? '' : '_outline_blank'}`)
 
 const deleteTask = (taskId) => {
   taskStore.deleteTask(taskId);
@@ -33,7 +33,7 @@ const _updateTask = async (fieldToUpdate) => {
   }
 
   await taskStore.updateTask(props.task.id, taskToUpdate);
-  
+
   if (_isEditing.value) {
     newTaskTitle.value = ''
     _isEditing.value = false
@@ -44,27 +44,46 @@ const _updateTask = async (fieldToUpdate) => {
 
 <template>
   <div class="task">
-    <h6>{{ task.title }}</h6>
-    <div class="icons">
-      <i class="material-icons" @click="deleteTask(task.id)">delete</i>
-      <i class="material-icons" :class="{ active: task.is_fav }" @click="_updateTask(TASK_PROPS.IS_FAV)">favorite</i>
-      <i class="material-icons" @click="_updateTask(TASK_PROPS.IS_COMPLETE)">{{ taskStatus }}</i>
-      <i class="material-icons" @click="_changeTitle">create</i>
+    <div class="task-content">
+      <h6>{{ task.title }}</h6>
+      <div class="icons">
+        <i class="material-icons" @click="deleteTask(task.id)">delete</i>
+        <i class="material-icons" :class="{ active: task.is_fav }" @click="_updateTask(TASK_PROPS.IS_FAV)">favorite</i>
+        <!-- <i class="material-icons" @click="_updateTask(TASK_PROPS.IS_COMPLETE)">{{ taskStatus }}</i> -->
+        <i class="material-icons" @click="_updateTask(TASK_PROPS.IS_COMPLETE)"
+          :class="{ active: task.is_complete }">
+          {{ props.task.is_complete ? 'check_box' : 'check_box_outline_blank' }}
+        </i>
+        <i class="material-icons" :class="{ active: _isEditing }" @click="_changeTitle">create</i>
+      </div>
     </div>
 
     <div v-show="_isEditing">
-      <label>
-        New Title
-        <input type="text" v-model="newTaskTitle" />
-      </label>
-      <button @click="_updateTask(TASK_PROPS.TITLE)">Update</button>
+      <div class="new-title d-flex align-items-center gap-2 w-100">
+        <input id="newTitle" type="text" v-model="newTaskTitle" />
+        <button @click="_updateTask(TASK_PROPS.TITLE)" class="btn btn-sm">Update</button>
+      </div>
     </div>
+
   </div>
+
 </template>
 
 <style scoped>
 /* task list */
 
+
+.new-title input {
+  width: 100%;
+  border: 0.5px solid lightgray;
+  /* Imposta uno spessore di 1 pixel e il colore grigio chiaro */
+  border-radius: 3px;
+  /* Aggiunge un piccolo arrotondamento ai bordi */
+  padding: 3px;
+  /* Aggiunge spazio interno per una migliore presentazione */
+  outline: none;
+  /* Rimuove l'outline predeterminato sul focus */
+}
 
 .task {
   padding: 6px 20px;
@@ -72,7 +91,9 @@ const _updateTask = async (fieldToUpdate) => {
   margin-top: 20px;
   border-radius: 4px;
   box-shadow: 2px 4px 6px rgba(0, 0, 0, 0.05);
+}
 
+.task-content {
   display: flex;
   justify-content: space-between;
   align-items: center;
